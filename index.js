b4ff89004340e7b3e637ef3e8c028ab580d0e055
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-11-21 21:52:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+var multiply = num1 * num2;
+console.log(multiply);
+
+const random= (Math.ceil(Math.random() * 10) + 1)
+
+
+let num3= 10;
+let num4=6;
+const mod = num3 % num4;
+console.log(mod);
+
+ const max = Math.max(12,13,14,15,16,17,18,19,20)
+
